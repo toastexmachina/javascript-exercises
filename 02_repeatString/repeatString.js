4af778e399aef20a913e0dda2fea8732ae555af5
toastexmachina <156368225+toastexmachina@users.noreply.github.com>
2024-05-07 13:49:31 -0500

    Write pseudocode outlining a for-loop that will concatenate two strings for num times.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    // create a loop that runs num times
+    for (i=0, i<num, i++){
 
+    }
+    // create a new string
+    let str1 = string.slice(); 
+
+    // concatenate the string
+    let string = string + str1;
 };
 
 // Do not edit below this line
