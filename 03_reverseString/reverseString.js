f6482c37e32221849ebbf0a6d5202920eebc5de8
toastexmachina <156368225+toastexmachina@users.noreply.github.com>
2024-05-08 12:54:59 -0500

    Write pseudocode

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
 const reverseString = function() {
 
+    // split string into an array
+    // Pop the last element and push into a new empty array
+    // repeat/loop until the array has been reversed
+    // convert array into a string
+
 };
 
 // Do not edit below this line
