f668800cbe59f7b11610683de23788c9e3ca2bda
toastexmachina <156368225+toastexmachina@users.noreply.github.com>
2024-05-09 15:51:06 -0500

    write pseudocode outlining the steps for a loop that will return the desired array

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,10 +1,28 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    
+    let array = string.split("");
+    let arrayReversed = [];
+    let index = array.slice(); 
+    for (i = 0; i < index.length; i++) {
+        arrayReversed.push(array.pop());
+    }
+    let stringReversed = arrayReversed.join("");
+    return stringReversed;
+    /*
     // split string into an array
+    array = string.split("");
+    // create new empty array
+    let arrayReversed = [];
     // Pop the last element and push into a new empty array
+        // arrayEmpty = array.pop()
+    arrayReversed.push(array.pop());
     // repeat/loop until the array has been reversed
+    for (i = 0; i < array.length, i++) {
+        arrayReversed.push(array.pop()); 
+    }
     // convert array into a string
-
+    let stringReversed = arrayReversed.join();
+    */
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, elements) {
+    // function to remove element from an array and 
+    // return the edited array
+
+    // loop
+    // indexOf to find specified element
+    // remove element each iteration
+    // when indexOf returns undefined -> break
+    // return edited array
 
 };
 
