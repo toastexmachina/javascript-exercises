b708d042b3f1d8249a380445a57d704962d45d86
toastexmachina <156368225+toastexmachina@users.noreply.github.com>
2024-05-10 13:41:45 -0500

    Compile pseudocode components

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -2,12 +2,25 @@ const removeFromArray = function(array, elements) {
     // function to remove element from an array and 
     // return the edited array
 
-    // loop
+    while true {
+        let index = array.indexOf(elements);
+        array.splice(index,1);
+        return array;
+    }
+
+    /*
+    // while loop
+    while true {
+
+    }
     // indexOf to find specified element
+    let index = array.indexOf(elements);    
     // remove element each iteration
+    array.splice(index, 1);
     // when indexOf returns undefined -> break
     // return edited array
-
+    return array;
+    */
 };
 
 // Do not edit below this line
