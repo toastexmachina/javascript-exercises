65238ff60e68b613582fc89f936e045bb5c81424
toastexmachina <156368225+toastexmachina@users.noreply.github.com>
2024-05-11 16:26:40 -0500

    Write preliminary pseudocode

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -3,24 +3,19 @@ const removeFromArray = function(array, ...elements) {
     // return the edited array
 
     for (element of elements) {
-        let index = array.indexOf(element);
-        if (index===-1) continue;
-        array.splice(index,1); 
+        while (true){
+            let index = array.indexOf(element);
+            if (index===-1) break;
+            array.splice(index, 1);
+        }
     }
 
     return array;
 
-    // Works only for single parameter
-    /*
-    while (true) {
-        let index = array.indexOf(element);
-        if (index === -1) break;
-        array.splice(index,1);
-    }
-
-    return array;
-    */
+    // loop that repeats an element until indexOf -> -1
 };
+
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,12 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
 
+    // create an array with all the numbers between and including num 1 and num 2
+
+    // loop for-of
+
+    // add all the elements in an array
+    
+    // return the sum
 };
 
 // Do not edit below this line
