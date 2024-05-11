f3423a3291d2d989a81e59540da3c4a76a9eafdc
toastexmachina <156368225+toastexmachina@users.noreply.github.com>
2024-05-11 17:01:44 -0500

    Replace min, max conditionals with array destructuring method to swap num1 and num2 if num1 is larger. and clean up code.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,12 +1,7 @@
 const sumAll = function(num1, num2) {
-    if (num1 < 0 || typeof num1 !== "number"){
-        return "ERROR";
-    }
-
-    if (num2 < 0 || typeof num2 !== "number"){
-        return "ERROR";
-    }
-
+    if (num1 < 0 || num2 < 0) return "ERROR";
+    if (typeof num1 !== "number" || typeof num2 !== "number") return "ERROR";
+/*
     let numberStart;
     let numberEnd;
     if (num1 < num2) {
@@ -16,9 +11,12 @@ const sumAll = function(num1, num2) {
         numberStart = num2; 
         numberEnd = num1; 
     }
+*/
+    if (num1 > num2) [num1, num2] = [num2, num1];
+    // array destructuring method
 
-    let sum = numberStart; 
-    for (i = numberStart+1; i<= numberEnd; i++) {
+    let sum = 0; 
+    for (i = num1; i<= num2; i++) {
         sum += i; 
     }
     return sum; 
