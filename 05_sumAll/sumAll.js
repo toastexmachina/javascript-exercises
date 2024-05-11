7260083adb5cbb112b0ac80d4d801e5f59996da7
toastexmachina <156368225+toastexmachina@users.noreply.github.com>
2024-05-11 16:40:07 -0500

    Write code that passes preliminary test for simplest parameters/

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,7 +1,9 @@
 const sumAll = function(num1, num2) {
-
-    // for loop starting with smaller number, ending with larger number
-    // sum each iteration
+    let sum = num1;
+    for (i = num1+1; i<= num2; i++) {
+        sum += i; 
+    }
+    return sum; 
 };
 
 // Do not edit below this line
